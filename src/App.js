042a88de78680c88e644dd64c78c4edf316cbac1
Vince Lu <vincentlu@hotmail.ca>
2022-01-28 18:42:12 -0500

    installed and set up redux-persist

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Plan4 from "./components/pages/plan4";
 import { auth } from "./firebase";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/"; //src/redux/index.js
+import store, { persistor } from "./redux/"; //src/redux/index.js
+import { PersistGate } from "redux-persist/integration/react";
 import AudioPlayer from "./components/pages/AudioPlayer/AudioPlayer";
 import AudioRecording from "./components/pages/AudioRecording/AudioRecording";
 import InternalRecording from "./components/pages/InternalRecording/InternalRecording";
@@ -37,29 +38,34 @@ function App() {
   }, []);
   return (
     <Provider store={store}>
-      <div className="App">
-        <Router>
-          {/*<UserProvider>*/}
-          {/*{user ? <Home /> : <Signin />}*/}
-          <Routes>
-            <Route path="/" element={<Signin />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/blog1" element={<BlogPage />} />
-            <Route path="/plan1" element={<Plan1 />} />
-            <Route path="/plan2" element={<Plan2 />} />
-            <Route path="/plan3" element={<Plan3 />} />
-            <Route path="/plan4" element={<Plan4 />} />
-            <Route path="/audioplayer" element={<AudioPlayer />} />
-            <Route path="/audiorecording" element={<AudioRecording />} />
-            <Route path="/internalrecording" element={<InternalRecording />} />
-            <Route path="/transcribe" element={<Transcribe />} />
-            <Route path="/systemaudio" element={<SystemAudio />} />
-            <Route path="/library" element={<Library />} />
-            <Route path="**" element={<Home />} />
-          </Routes>
-          {/*</UserProvider>*/}
-        </Router>
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="App">
+          <Router>
+            {/*<UserProvider>*/}
+            {/*{user ? <Home /> : <Signin />}*/}
+            <Routes>
+              <Route path="/" element={<Signin />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/blog1" element={<BlogPage />} />
+              <Route path="/plan1" element={<Plan1 />} />
+              <Route path="/plan2" element={<Plan2 />} />
+              <Route path="/plan3" element={<Plan3 />} />
+              <Route path="/plan4" element={<Plan4 />} />
+              <Route path="/audioplayer" element={<AudioPlayer />} />
+              <Route path="/audiorecording" element={<AudioRecording />} />
+              <Route
+                path="/internalrecording"
+                element={<InternalRecording />}
+              />
+              <Route path="/transcribe" element={<Transcribe />} />
+              <Route path="/systemaudio" element={<SystemAudio />} />
+              <Route path="/library" element={<Library />} />
+              <Route path="**" element={<Home />} />
+            </Routes>
+            {/*</UserProvider>*/}
+          </Router>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers } from "redux";
-
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist"; // imports from redux-persist
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import recordingAudioReducer from "./recording/recordingAudioReducer";
 import userReducer from "./user/userReducer";
 import languageReducer from "./language/languageReducer";
@@ -10,6 +11,16 @@ const allReducers = combineReducers({
   languageReducer: languageReducer,
 });
 
-const store = createStore(allReducers);
+const persistConfig = {
+  // configuration object for redux-persist
+  key: "root",
+  storage, // define which storage to use
+};
+
+const persistedReducer = persistReducer(persistConfig, allReducers); // create a persisted reducer
+
+const store = createStore(persistedReducer, applyMiddleware());
+const persistor = persistStore(store); // used to create the persisted store, persistor will be used in the next step
 export default store;
+export { persistor };
 //export default createStore(allReducers);
